9dfd6d87d8e5e3a09c412004d69af3251b6ff2cf
磊磊 <leiwuMail@163.com>
2017-11-28 16:10:23 +0800

    first record

---
+++ maibei_temporay_swift/maibei_temporay_swiftTests/maibei_temporay_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  maibei_temporay_swiftTests.swift
+//  maibei_temporay_swiftTests
+//
+//  Created by l.wu on 2017/11/28.
+//  Copyright © 2017年 l.wu. All rights reserved.
+//
+
+import XCTest
+@testable import maibei_temporay_swift
+
+class maibei_temporay_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
